05b34dc5c6570c3b747306bb7bda9196acfdd4f2
adel KACIMI <adel.kacimi@free.fr>
2019-07-22 10:37:46 +0200

    send user by id

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/users', (req, res) => {
     const users = require('./fixtures').users;
-    res.status(200).send({users});
+    res.status(200).send({ users });
 })
 
-app.listen(PORT, 
+app.get('/user/:id', (req, res) => {
+    const userId = req.params.id;
+    const user = require('./fixtures').users[userId];
+
+    if (!user) res.status(404).send({ message: `L'utilisateur n'existe pas` });
+    else
+        res.status(200).send({ user });
+})
+app.listen(PORT,
     () => console.log(`Server listening on ${PORT}`));
